fe943aef598c2866f619f7e69fea795441da1c8d
jvbads <jvbads@gmail.com>
2022-08-10 17:33:04 -0300

    Department CRUD Created

---
+++ SalesDashboard/Data/SalesDashboardContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesDashboard.Models;
+
+namespace SalesDashboard.Data
+{
+    public class SalesDashboardContext : DbContext
+    {
+        public SalesDashboardContext (DbContextOptions<SalesDashboardContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesDashboard.Models.Departments> Departments { get; set; }
+    }
+}
---
+++ SalesDashboard/Models/Departments.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesDashboard.Models
+{
+    public class Departments
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
